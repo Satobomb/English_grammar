86dd3ddc269f962f72c4249cb12ec72913728879
SatoBomb <takafumi9653@docomo.ne.jp>
2021-10-22 17:01:31 +0900

    feat:addcontents

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var page = 1;
 var allText = document.getElementById('allText').getElementsByTagName('div');
-//var _tts;
+var _tts;
+var text1 = "hello NAO";//"he plays baseball";//very well."; //he seems to be good at studying too.";
 
-function clickBtnNext(){    
+function clickBtnNext(){     //ページを進める
     if(page >= allText.length) return;
     for(var i=1; i<allText.length; i++) {
         if(i == page){
@@ -13,7 +14,7 @@ function clickBtnNext(){
     page++;
     //console.log(page);
 }
-function clickBtnBack(){    
+function clickBtnBack(){    //ページを戻す
     if(page <= 1) return;
     for(var i=2; i<=allText.length; i++) {
         if(i == page){
@@ -23,8 +24,9 @@ function clickBtnBack(){
     }
     page--;
 }
-allText[0].style.display = "block";
+allText[0].style.display = "block"; //1ページ目を表示
 
+//↓Naoを動かす用
 /*
 var session = new QiSession("192.168.1.14:80");
     session.socket().on('connect', function () {
@@ -65,10 +67,11 @@ var session = new QiSession("192.168.1.14:80");
             });
         });
     });
-
-    */
+*/
     function clickBtn(){   
-        //_tts.say("hello");
-        console.log("hello");
+        var text = text1;
+        //console.log(text);
+        //_tts.say("I play tennis");
+        //_tts.say(text);
     }
     
