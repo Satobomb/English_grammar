e922c319d9133217faf6fa35d8be10a4b7628d8b
SatoBomb <takafumi9653@docomo.ne.jp>
2021-10-25 16:52:26 +0900

    feat:speakBothJAandEN

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var page = 1;
 var allText = document.getElementById('allText').getElementsByTagName('div');
 var _tts;
-var text1 = "hello NAO";//"he plays baseball";//very well."; //he seems to be good at studying too.";
+var _lang;
+var jaText = "彼は野球が上手だよ。";
+var enText = "he plays baseball very well."; //he seems to be good at studying too.";
 
 function clickBtnNext(){     //ページを進める
     if(page >= allText.length) return;
@@ -12,7 +14,6 @@ function clickBtnNext(){     //ページを進める
         }
     }
     page++;
-    //console.log(page);
 }
 function clickBtnBack(){    //ページを戻す
     if(page <= 1) return;
@@ -27,7 +28,7 @@ function clickBtnBack(){    //ページを戻す
 allText[0].style.display = "block"; //1ページ目を表示
 
 //↓Naoを動かす用
-/*
+
 var session = new QiSession("192.168.1.14:80");
     session.socket().on('connect', function () {
         console.log('QiSession connected!');
@@ -37,16 +38,25 @@ var session = new QiSession("192.168.1.14:80");
     });
 
     session.service("ALTextToSpeech").done((tts) => {
-        _tts = tts;        
-    }).fail((error) => {
+        _tts = tts;   
+        
+       
+
+        _tts.getLanguage().done(function (lang) { //言語の取得
+            console.log("language is " + lang + " now");
+            _lang = lang;
+        }).fail(function (error) {
             console.log("An error occurred: " + error);
+        });
+        
+    }).fail((error) => {
+        console.log("An error occurred: " + error);
     });
 
     var signalLink;
     var serviceDirectory;
 
-    function onServiceAdded(serviceId, serviceName)
-    {
+    function onServiceAdded(serviceId, serviceName){
         console.log("New service", serviceId, serviceName);
         serviceDirectory.serviceAdded.disconnect(signalLink);
     }
@@ -59,6 +69,7 @@ var session = new QiSession("192.168.1.14:80");
             console.log("An error occurred: " + error);
         });
     });
+
     session.service("ALMemory").done(function (ALMemory) {
         ALMemory.subscriber("FrontTactilTouched").done(function (subscriber) {
             // subscriber.signal is a signal associated to "FrontTactilTouched"
@@ -67,11 +78,21 @@ var session = new QiSession("192.168.1.14:80");
             });
         });
     });
-*/
-    function clickBtn(){   
-        var text = text1;
-        //console.log(text);
-        //_tts.say("I play tennis");
-        //_tts.say(text);
+
+    function speakJapanese(){   
+        var text = jaText;
+        console.log(text);
+        _tts.setLanguage("Japanese").done().fail(function (error) { //言語の設定
+            console.log("An error occurred: " + error);
+        });
+        _tts.say(text);
     }
     
+    function speakEnglish(){   
+        var text = enText;
+        console.log(text);
+        _tts.setLanguage("English").done().fail(function (error) { //言語の設定
+            console.log("An error occurred: " + error);
+        });
+        _tts.say(text);
+    }
