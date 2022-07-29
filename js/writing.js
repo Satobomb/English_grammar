9be0564c2f9e6bd9ea152b480b7ed72a5fd61975
SatoBomb <takafumi9653@docomo.ne.jp>
2022-07-29 14:29:01 +0900

    refactor:htmlとjsファイルの調整

--- js/writing.js
+++ js/writing.js
@@ -1,3 +1,4 @@
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 let socket = io.connect();
 
 socket.on("DISPLAY_SENTENCES", (text) => {
@@ -8,12 +9,24 @@ socket.on("BACK_TO_TOPPAGE", () => {
     location.href = "/";
 });
 
+//let btn = document.getElementById("btn");
+
+// window.document.onkeydown = function(event){
+//     if (event.key === "Enter") {
+//         document.Form.submit();
+//     }
+// }
+
+
+
 function pre_writingStart(){
     socket.emit("WRITING_TO_SERVER");
 }
 
 function clickButton(){
-    const text = document.form.textBox.value;
-    document.form.textBox.value = "";
-    socket.emit('ANSWERED', (text));
+    const text = document.Form.textBox.value;
+    if(text != ""){
+        document.Form.textBox.value = "";
+        socket.emit('ANSWERED', (text));
+    }
 }
