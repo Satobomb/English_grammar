522801a2f93065e3106abbb653ebd17cb5dacfb8
SatoBomb <takafumi9653@docomo.ne.jp>
2022-10-21 19:50:17 +0900

    feat:発話テストの実装

--- js/speaking.js
+++ js/speaking.js
@@ -1,5 +1,6 @@
 var v = document.getElementById('video');
 
+//再生バーを消す
 // jQuery(function($){
 //     v.controls = false;
 // });
@@ -7,6 +8,7 @@ var v = document.getElementById('video');
 function play(){ v.play(); }
 function pause(){ v.pause(); }
 
+//特定の時間で止まる
 // v.addEventListener('timeupdate', function() {
 //     if(v.currentTime >= 5){ v.pause(); }
 // 	console.log(v.currentTime);
